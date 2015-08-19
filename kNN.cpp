31dc67f9f52606872f4d12ca3960da4652c6d707
Dung Phan <brianp.dung@gmail.com>
2015-08-19 11:30:28 -0500

    Testing Data class.

--- kNN.cpp
+++ kNN.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <cmath>
 #include <string>
-#include <fstream>
+
+#include "Data.h"
+//#include "kNNAlgorithm.h"
+/* ----------------------------------------------------------------------------------------- */
 
 /*---Main kNN program ---*/
 int main() {
+	Data* ValidationData = new Data();
+
+	ValidationData->ImportData("./iris.data");
+	std::vector<DataStructure>	TrainData = ValidationData->GetTrainPoints();
+	std::vector<DataStructure>	TestData  = ValidationData->GetTestPoints();
+	int NTest  = ValidationData->GetNTest();
+	int NTrain = ValidationData->GetNTrain();
+
+	for (int i = 0; i < NTest; i++) {
+		std::cout << "Test point: " << i << "\t";
+		std::cout << "True ID: " << TestData.at(i).ClassTrueID << std::endl;
+	}
+
 	return 0;
 }
